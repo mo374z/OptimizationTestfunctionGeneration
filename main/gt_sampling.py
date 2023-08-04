e5a47ae67da0b8c76dd9bdd43004a0f2ca093c1a
mo374z <schlager.mo@t-online.de>
2023-08-04 11:17:01 +0200

    Change sampling functions

--- main/gt_sampling.py
+++ main/gt_sampling.py
@@ -31,23 +31,14 @@ def random_sample(n_samples, n_dim, seed, lower_bound=-3, upper_bound=5):
     samples_random = np.random.uniform(low=lower_bound, high=upper_bound, size=(n_samples, n_dim))
     return samples_random
 
-def get_sample(f_number, n_samples, n_dim, seed=42, lower_bound=-5, upper_bound=5, method='random', particles=20, iters=250, options={'c1': 0.8, 'c2': 0.3, 'w': 0.95}):
-    if f_number == 1:
-        problem = bbobtorch.create_f01(n_dim, seed=42)
-    elif f_number == 2:
-        problem = bbobtorch.create_f03(n_dim, seed=42)
-    elif f_number == 3:
-        problem = bbobtorch.create_f24(n_dim, seed=42)
-    else:
-        raise ValueError('We only use BBOB functions be 1, 3 and 24')
-
+def get_sample(problem, n_samples, n_dim, seed=42, lower_bound=-5, upper_bound=5, method='random', particles=20, iters=250, options={'c1': 0.8, 'c2': 0.3, 'w': 0.95}):
     if method == 'random':
         result = random_sample(n_samples, n_dim, seed, lower_bound, upper_bound)
-        return result, problem(torch.tensor(result, dtype=torch.float64).float())
     elif method == 'pso':
         result = sample_from_problem_with_pso(problem, n_dim, seed, n_samples=n_samples, particles=particles, iters=iters, options=options, lower_bound=lower_bound, upper_bound=upper_bound)
-        return result, problem(torch.tensor(result, dtype=torch.float64).float())
     else:
         raise ValueError('method must be either random or pso')
+    
+    return result, problem(torch.tensor(result, dtype=torch.float64).float())
     
 
