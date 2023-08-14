a37be00aa9db2da2f5d76fd26e453be727f1a108
mo374z <schlager.mo@t-online.de>
2023-08-14 14:09:51 +0200

    Add second derivative

--- main/gt_sampling.py
+++ main/gt_sampling.py
@@ -47,6 +47,25 @@ def get_gradient(problem, x, h=1e-5):
         gradient.append(partial_derivative.item())
     return gradient
 
+def get_second_derivative(problem, x, h=1e-5):
+    second_derivative = []
+    for i in range(len(x[0])):
+        # Create perturbed points for the i-th dimension
+        perturbed_x_left = x.clone()
+        perturbed_x_right = x.clone()
+        perturbed_x_left[0][i] -= h
+        perturbed_x_right[0][i] += h
+        
+        # Calculate function values at the original and perturbed points
+        y_original = problem(x)
+        y_left = problem(perturbed_x_left)
+        y_right = problem(perturbed_x_right)
+        
+        # Calculate finite difference approximation of the second derivative
+        second_derivative_partial = (y_left - 2 * y_original + y_right) / (h ** 2)
+        second_derivative.append(second_derivative_partial.item())
+    return second_derivative
+
 def get_sample(problem, n_samples, n_dim, seed=42, lower_bound=-5, upper_bound=5, method='random', particles=20, iters=250, options={'c1': 0.8, 'c2': 0.3, 'w': 0.95}):
     if method == 'random':
         result = random_sample(n_samples, n_dim, seed, lower_bound, upper_bound)
@@ -56,10 +75,16 @@ def get_sample(problem, n_samples, n_dim, seed=42, lower_bound=-5, upper_bound=5
         raise ValueError('method must be either random or pso')
     
     gradients = np.array([])
+    second_derivatives = np.array([])
+
     for i in range(n_samples):
-        gradients = np.append(gradients, get_gradient(problem, torch.tensor(result[i], dtype=torch.float64).reshape((1, n_dim)).float()))
+        tensor_sample = torch.tensor(result[i], dtype=torch.float64).reshape((1, n_dim)).float()
+        gradient = get_gradient(problem, tensor_sample)
+        second_derivative = get_second_derivative(problem, tensor_sample)
+        gradients = np.append(gradients, gradient)
+        second_derivatives = np.append(second_derivatives, second_derivative)
 
     gradients = gradients.reshape((n_samples, n_dim))
+    second_derivatives = second_derivatives.reshape((n_samples, n_dim))
     
-    return result, problem(torch.tensor(result, dtype=torch.float64).float()), gradients
-
+    return result, problem(torch.tensor(result, dtype=torch.float64).float()), gradients, second_derivatives
