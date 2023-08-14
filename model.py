11da7eea2342c1f3d30906b7ba4656e6126ffb6a
Timo Heiss <ti-heiss@t-online.de>
2023-08-14 16:30:27 +0200

    Add normalized NN

--- model.py
+++ model.py
@@ -19,6 +19,31 @@ class NN(torch.nn.Module):
             x = layer(x)
         return x
 
+
+class NN_norm(torch.nn.Module):
+    def __init__(self, n_in, n_hidden, hidden_layers):
+        super().__init__()
+
+        # Define the normalization layer
+        self.normalize = torch.nn.BatchNorm1d(n_in)
+
+        self.layers = torch.nn.ModuleList()
+        for _ in range(hidden_layers):
+            self.layers.append(torch.nn.Linear(n_in, n_hidden))
+            self.layers.append(torch.nn.ReLU())
+            n_in = n_hidden
+
+        self.layers.append(torch.nn.Linear(n_hidden, 1))
+
+    def forward(self, x):
+        # Apply input normalization
+        x = self.normalize(x)
+
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
 # create class for NN with outputdim = 1
 class NN1(torch.nn.Module):
     def __init__(self, n_in, n_hidden, hidden_layers):
