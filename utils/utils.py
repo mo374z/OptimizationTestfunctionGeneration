3d85f82d9bb396d86c196d0f66f239a09d3edf72
Pascal Knoll <knollpascal00@gmail.com>
2023-09-16 23:07:15 +0200

    add some comments for utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -24,7 +24,6 @@ def create_problem(f_number, n_dim, seed):
 
     return problem
 
-
 def plot_sampled_data(samples, results, f_number: str):	
     plt.scatter(samples[:, 0], samples[:, 1], c=results, cmap='inferno', s=1)
 
@@ -39,7 +38,9 @@ def plot_sampled_data(samples, results, f_number: str):
     return plt.gca()
 
 
+
 def plot_simulated_meshgrid(X, Y, mesh_results, model: str, colorbar=True):
+    # Create a heatmap-like plot of simulated function values on a meshgrid defined by X and Y
     plt.pcolormesh(X, Y, mesh_results, cmap='inferno', shading='nearest')
     if colorbar: plt.colorbar(label='Function Value')
     plt.xlabel('$x_{1}$')
@@ -72,14 +73,14 @@ def plot_ground_truth(n_dim, problem, f_name, xlim=(-5, 5), step=0.01):
 
 
 def plot_collage(samples, results, problem, problem_name, model_name, X, Y, mesh_results):
-    # Plot the sampled points
+    
     plt.figure(figsize=(14, 6))
-
     plt.subplot(1, 3, 1)
+    # Plot the sampled points
     plot_sampled_data(samples, results, problem_name)
 
-    # Plot the predicted function values using a contour plot
     plt.subplot(1, 3, 2)
+    # Plot the predicted function values using a contour plot
     plot_simulated_meshgrid(X, Y, mesh_results, model_name)
 
     # Plot the ground truth using a contour plot
@@ -89,45 +90,26 @@ def plot_collage(samples, results, problem, problem_name, model_name, X, Y, mesh
     plt.tight_layout()
     return plt.gca()
 
-# def euclidean_distance(point1, point2):
-#     return np.sqrt(np.sum((np.array(point1) - np.array(point2)) ** 2))
 
-# def nearest_point(x1, x2, point_set):
-#     min_distance = float('inf')
-#     nearest_point = None
-    
-#     for point in point_set:
-#         distance = euclidean_distance([x1, x2], point)	
-#         if distance < min_distance:
-#             min_distance = distance
-#             nearest_point = point
-    
-#     return nearest_point
+def plot_collage_results(problem, problem_name, model_name, model_name_2, X, Y, mesh_results, mesh_results_2):
+    # Create a plot colage with mesh results from model 1, ground truth and mesh results from model 2
+    # Plot the sampled points
+    plt.figure(figsize=(14, 6))
 
-# def nearest_point_with_target(x1, x2, point_set, target):
-#     min_distance = float('inf')
-#     nearest_point = None
-#     target_value = None
-    
-#     for point, target_val in zip(point_set, target):
-#         distance = euclidean_distance([x1, x2], point)	
-#         if distance < min_distance:
-#             min_distance = distance
-#             nearest_point = point
-#             target_value = target_val
-    
-#     return nearest_point, target_value
+    plt.subplot(1, 3, 1)
+    # Plot the predicted function of model 1 using a contour plot
+    plot_simulated_meshgrid(X, Y, mesh_results, model_name)
 
+    plt.subplot(1, 3, 2)
+    # Plot the ground truth using a contour plot
+    plot_ground_truth(2, problem, f_name=problem_name)
 
-# def test_function(X, X_train, X_train_grads, model):
-#     ''' X 2D array of shape (2, n) '''
-#     X_grads = np.zeros_like(X)
-#     for i in range(len(X)):
-#         X_grads[i, 0], X_grads[i, 1] = nearest_point_with_target(X[i,0], X[i,1], X_train, X_train_grads)[1]
-    
-#     X_in = np.concatenate((X, X_grads), axis=1)
-#     return model.predict(X_in)
+    plt.subplot(1, 3, 3)
+     # Plot the predicted function of model 2 using a contour plot
+    plot_simulated_meshgrid(X, Y, mesh_results_2, model_name_2)
 
+    plt.tight_layout()
+    return plt.gca()
 
 def test_function(X, X_train, X_train_grads, model, method='nearest_neighbor', gradient_estimator=None):
     if method=='nearest_neighbor':
@@ -150,8 +132,8 @@ def test_function(X, X_train, X_train_grads, model, method='nearest_neighbor', g
     
     return predictions
 
-
-def calculate_eval_metrics(functions:list, optims:list, n_trials, n_dim=2, max_iters_optim=100, seed=42, epsilon=5e-4):
+def calculate_eval_metrics(functions:list, optims:list, n_trials, n_dim=2,  seed=42, epsilon=5e-4): #max_iters_optim=100, XXX check if this is needed
+    
     df_nr_iter = pd.DataFrame(columns=[f[1] for f in functions])
     df_optim_loc = pd.DataFrame(columns=[f[1] for f in functions])
     df_optim_val = pd.DataFrame(columns=[f[1] for f in functions])
@@ -171,7 +153,7 @@ def calculate_eval_metrics(functions:list, optims:list, n_trials, n_dim=2, max_i
             for i in range(n_trials):
                 eval_seed = torch.randint(0, 1000, (1,)).item()
                 res = []
-                res = perform_optimization(optimization_type, function[0], n_dim=2, num_iterations=100, seed=eval_seed, epsilon=epsilon)
+                res = perform_optimization(optimization_type, function[0], n_dim=n_dim, num_iterations=100, seed=eval_seed, epsilon=epsilon)
                 trials_iters.append(len(res[1]))
                 trials_optim_loc.append(res[0][-1] if type(res[0][-1]) == np.ndarray else res[0][-1].numpy())
                 trials_optim_val.append(res[1][-1])
