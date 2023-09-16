611158633859fcc8bf5df44965afabadcd324f67
Timo Heiss <ti-heiss@t-online.de>
2023-09-16 23:08:57 +0200

    Refactor sampling_test

--- utils/gt_sampling.py
+++ utils/gt_sampling.py
@@ -7,7 +7,7 @@ import pyswarms as ps
 def sample_from_problem_with_pso(problem, 
                                  n_dim, 
                                  seed, 
-                                 n_samples=5000, 
+                                 n_samples=1000, 
                                  particles=20, 
                                  iters=250, 
                                  options={'c1': 0.8, 'c2': 0.3, 'w': 0.95},
@@ -26,7 +26,7 @@ def sample_from_problem_with_pso(problem,
     return samples
 
 
-def random_sample(n_samples, n_dim, seed, lower_bound=-3, upper_bound=5):
+def random_sample(n_samples, n_dim, seed, lower_bound=-5, upper_bound=5):
     np.random.seed(seed)
     samples_random = np.random.uniform(low=lower_bound, high=upper_bound, size=(n_samples, n_dim))
     return samples_random
